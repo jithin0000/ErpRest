c9d514b0c12e2e8e4ca786400d1a7f93f2833541
psabhjith <psabhjith@bitbucket.org>
2019-02-22 17:01:12 +0530

    removed Hateos links

--- src/main/java/com/example/demo/controller/InstitutionController.kt
+++ src/main/java/com/example/demo/controller/InstitutionController.kt
@@ -4,12 +4,7 @@ import com.example.demo.exceptions.InstitutionNotFoundException
 import com.example.demo.model.Institution
 import com.example.demo.repository.InstitutionRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.hateoas.Resource
-import org.springframework.hateoas.Resources
-import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
-import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
 import org.springframework.web.bind.annotation.*
-import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/institution")
@@ -18,22 +13,9 @@ class InstitutionController(@field:Autowired
 
 
     @GetMapping("/all")
-    fun getAllInstitutionList(): Resource<MutableList<Resource<Institution>>> {
+    fun getAllInstitutionList(): List<Institution>{
 
-        val institutions =   institutionRepository.findAll().stream()
-                .map {
-                    inst -> Resource(
-                        inst,
-                        linkTo(methodOn(InstitutionController::class.java).getInstitutionDetailsById(inst.id)).withSelfRel(),
-                        linkTo(methodOn(InstitutionController::class.java).getAllInstitutionList()).withRel("institutions")
-                )
-                }.collect(Collectors.toList())
-
-        return Resource(
-                institutions,
-                linkTo(methodOn(InstitutionController::class.java).getAllInstitutionList()).withSelfRel()
-        )
-
+        return institutionRepository.findAll()
 
     }
 
@@ -44,16 +26,12 @@ class InstitutionController(@field:Autowired
 
     }
 
-    @GetMapping("/inst/{id}")
-    fun getInstitutionDetailsById(@PathVariable id: Long): Resource<Institution> {
+    @GetMapping("/{id}")
+    fun getInstitutionDetailsById(@PathVariable id: Long): Institution {
 
-        val institution = institutionRepository.findById(id).orElseThrow { InstitutionNotFoundException(id) }
+        return institutionRepository.findById(id).orElseThrow { InstitutionNotFoundException(id) }
 
-        return Resource(
-                institution,
-                linkTo(methodOn(InstitutionController::class.java).getInstitutionDetailsById(id)).withSelfRel(),
-                linkTo(methodOn(InstitutionController::class.java).getAllInstitutionList()).withRel("institutions")
-        )
+
     }
 
 
